014dc570fed534a8dc6ff4a44fb9ff0fb68a1165
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-20 19:18:02 -0300

    função criar atividade
    Função para adicionar atividade finalizada

--- main.js
+++ main.js
@@ -8,4 +8,15 @@ const paleta2 = document.querySelector(".paleta2");
 const paleta3 = document.querySelector(".paleta3");
 
 
-fun
+function criaAtividade(){
+    const atividade = document.createElement("div");
+    atividade.classList.add("atividade");
+    const nomeAtividade = document.createElement("p");
+    atividade.textContent = input.value;
+    const botaoLimpar = document.createElement("button");
+    botaoLimpar.textContent = "Limpar";
+    botaoLimpar.classList.add("botao_del");
+    listaAtividades.appendChild(atividade);
+    atividade.appendChild(nomeAtividade);
+    atividade.appendChild(botaoLimpar);
+};
